c2404607e068fc4c83927693a5b57fcb2394ea14
LucasGeneroso15 <lucas.siilva12@live.com>
2024-09-21 10:31:44 -0300

    06 math

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controller/GreetingController.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controller/GreetingController.java
@@ -1,8 +1,7 @@
-package br.com.erudio;
+package br.com.erudio.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
